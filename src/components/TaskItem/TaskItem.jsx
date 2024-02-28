8519304dd9d86ca4d906bc73e5b0fe9d3f480f9c
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-02-28 18:02:45 +0100

    more styling

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -3,23 +3,28 @@
 import React from 'react';
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { MdOutlineModeEditOutline } from "react-icons/md";
+import { MdFileDownloadDone } from "react-icons/md";
 
 
 
 const TaskItem = ({ task, onEdit, onDelete, onComplete }) => {
   return (
-    <li style={{ display: 'flex' , flexDirection:'column' }}>
+    <li >
         <div>
           <h3>{task.name}</h3>
-            <div>
-                  {task.completed ? <p>Completed</p> : <button onClick={() => onComplete(task.id)}>Complete</button>}
-      <button onClick={() => onEdit(task.id)}><MdOutlineModeEditOutline /></button>
+            <div >
+                  {task.completed ? <p>Completed</p> : <button style={{marginRight:'10px'  }} onClick={() => onComplete(task.id)}><MdFileDownloadDone /></button>}
+      <button  style={{marginRight:'10px'  }}  onClick={() => onEdit(task.id)}><MdOutlineModeEditOutline /></button>
       <button onClick={() => onDelete(task.id)}><RiDeleteBin6Line /></button>
             </div>
         </div>
-        <div><p>{task.description}</p></div>
+        <div style={{ maxWidth: '80%' , flexDirection:'column' }}>
+            <p>{task.description}</p>
+         <p>Due Date: {task.dueDate}</p>
+        
+        </div>
    
-                <p>Due Date: {task.dueDate}</p>
+               
       
  
     
